c49bdaf71657008582dc6bde909d7741070603f6
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2026-05-19 20:13:30 +0530

    feat: create url obj

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package cmd
+
+import "net/url"
+
+type monsql struct {
+	mongoURL string
+}
+
+func main() {
+	m := monsql{
+		mongoURL: "",
+	}
+	_ = m // prevent unused variable error if main is run
+}
+
+func (m *monsql) create(rawURL string) error {
+	sanitized, err := validateAndSanitizeURL(rawURL)
+	if err != nil {
+		return err
+	}
+	m.mongoURL = sanitized
+	return nil
+}
+
+func validateAndSanitizeURL(rawURL string) (string, error) {
+	sanitized, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+	return sanitized.String(), nil
+}
